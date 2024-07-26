e23e11bacd933e528e441ff5636cfc05cbef2240
sevenf0ld <115096194+sevenf0ld@users.noreply.github.com>
2024-07-26 19:45:01 +0800

    modify compare_struct() and original Data obj to Data *obj
    * pass a pointer to serialize() instead of the address for clear demonstration of deserialize() working as expected

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,43 +6,66 @@
 /*   By: maiman-m <maiman-m@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 10:53:14 by maiman-m          #+#    #+#             */
-/*   Updated: 2024/04/17 11:23:40 by maiman-m         ###   ########.fr       */
+/*   Updated: 2024/07/26 19:42:47 by maiman-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "zero_one.h"
 
+// reference parameters are initialized with the actual arguments when the function is called.
+// (source: https://www.ibm.com/docs/en/zos/2.4.0?topic=calls-pass-by-reference-c-only)
+/*
 void compare_struct(Data &ref, Data &new_ptr)
 {
-	std::cout << AC_YELLOW << &ref << AC_NORMAL << std::endl
+	std::cout << AC_YELLOW << &ref << "\t<obj>" << AC_NORMAL << std::endl
 			  << "continent: " << ref.continent << std::endl
 			  << "region: " << ref.region << std::endl
 			  << "country: " << ref.country << std::endl;
 
-	std::cout << AC_CYAN << &new_ptr << AC_NORMAL << std::endl
-			  << "continent: " << ref.continent << std::endl
-			  << "region: " << ref.region << std::endl
-			  << "country: " << ref.country << std::endl;
+	std::cout << AC_CYAN << &new_ptr << "\t<*ret>" << AC_NORMAL << std::endl
+			  << "continent: " << new_ptr.continent << std::endl
+			  << "region: " << new_ptr.region << std::endl
+			  << "country: " << new_ptr.country << std::endl;
+}
+*/
+
+void compare_struct(Data *ref, Data *new_ptr)
+{
+	std::cout << AC_YELLOW << ref << "\t`obj`" << AC_NORMAL << std::endl
+			  << "continent: " << ref->continent << std::endl
+			  << "region: " << ref->region << std::endl
+			  << "country: " << ref->country << std::endl;
+
+	std::cout << AC_CYAN << new_ptr << "\t`*ret`" << AC_NORMAL << std::endl
+			  << "continent: " << new_ptr->continent << std::endl
+			  << "region: " << new_ptr->region << std::endl
+			  << "country: " << new_ptr->country << std::endl;
 }
 
 int main(void)
 {
-	Data obj;
-	Data *ret;
+	Data *obj = new Data;
 
-	obj.continent = "Asia";
-	obj.region = "SEA";
-	obj.country = "Malaysia";
+	obj->continent = "Asia";
+	obj->region = "SEA";
+	obj->country = "Malaysia";
+
 	// Use serialize() on the address of the Data object and pass its return value to deserialize(). Then, ensure the return value of deserialize() compares equal to the original pointer.
-	ret = Serializer::deserialize(Serializer::serialize(&obj));
-	compare_struct(obj, *ret);
+	Data *ret = Serializer::deserialize(Serializer::serialize(obj));
+	compare_struct(obj, ret);
 	std::cout << std::endl;
+
+	std::cout << AC_BOLD << "Modifying data members of `Data *ret`..." << AC_NORMAL << std::endl << std::endl;
 
 	ret->continent = "Australia";
 	ret->region = "Oceania";
 	ret->country = "Australia";
-	compare_struct(obj, *ret);
+	compare_struct(obj, ret);
 	std::cout << std::endl;
+
+	std::cout << AC_BOLD << "Modifications to `Data *ret` reflected in `Data obj`, an indication that the retval of `deserialize()` does compare equal to the original pointer passed to it." << AC_NORMAL << std::endl << std::endl;
+
+	delete obj;
 
 	// Serializer test;
 }
